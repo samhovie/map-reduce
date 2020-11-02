4853109ab2dcbb7c9776430c941f0a49bfd595e6
Unser Hashsham <unserh@umich.edu>
2020-11-01 20:59:02 -0500

    Clean up initial worker code

--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -5,40 +5,57 @@ import time
 import click
 import mapreduce.utils
 
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Worker:
+    def __init__(self, master_port, worker_port):
+        logging.info("Starting worker:%s", worker_port)
+        logging.info("Worker:%s PWD %s", worker_port, os.getcwd())
+
+        # Listen for messages on worker's port
+        thread = threading.Thread(target=listen, args=(worker_port,))
+        thread.start()
+
+        # Connect to the server
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("localhost", master_port))
+
+        # Send register message to master
+        message = json.dumps({
+            "message_type" : "register",
+            "worker_host" : "localhost",
+            "worker_port" : worker_port,
+            "worker_pid" : id
+        })
+        sock.sendall(message.encode('utf-8'))
+        sock.close()
+
 def listen(port):
     """Wait on a message from a socket OR a shutdown signal."""
-    # print("listen() starting")
 
-    # Create an INET, STREAMing socket, this is TCP
+    # Create socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Bind the socket to the server
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(("localhost", port))
     sock.listen()
 
-    # Socket accept() and recv() will block for a maximum of 1 second.  If you
-    # omit this, it blocks indefinitely, waiting for a connection.
+    # Socket accept() and recv() will block for a maximum of 1 second
     sock.settimeout(1)
 
-    # while not signals["shutdown"]:
+    # Accept connections for messages until shutdown message
     shutdown = False 
     while not shutdown:
-        # print("listening")
-
-        # Listen for a connection for 1s.  The socket library avoids consuming
-        # CPU while waiting for a connection.
+        # Listen for a connection for 1s
         try:
             clientsocket, address = sock.accept()
         except socket.timeout:
             continue
         print("Connection from", address[0])
 
-        # Receive data, one chunk at a time.  If recv() times out before we can
-        # read a chunk, then go back to the top of the loop and try again.
-        # When the client closes the connection, recv() returns empty data,
-        # which breaks out of the loop.  We make a simplifying assumption that
-        # the client will always cleanly close the connection.
+        # Receive data chunks
         message_chunks = []
         while True:
             try:
@@ -50,7 +67,7 @@ def listen(port):
             message_chunks.append(data)
         clientsocket.close()
 
-        # Decode list-of-byte-strings to UTF8 and parse JSON data
+        # Parse message chunks into JSON data
         message_bytes = b''.join(message_chunks)
         message_str = message_bytes.decode("utf-8")
         try:
@@ -58,55 +75,11 @@ def listen(port):
         except JSONDecodeError:
             continue
 
-        if msg["message_type"] == "shutdown":
-            
-            
+        # Handle message depending on type
+        if msg["message_type"] == "shutdown":            
             shutdown = True
-
-
-        # message_dict = json.loads(message_str)
-        # print(message_dict)
-
-
-
-
-
-    # print("listen() shutting down")
-
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
-
-
-class Worker:
-    def __init__(self, master_port, worker_port):
-        logging.info("Starting worker:%s", worker_port)
-        logging.info("Worker:%s PWD %s", worker_port, os.getcwd())
-
-        id = os.getpid
-
-        thread = threading.Thread(target=listen, args=(worker_port,))
-        thread.start()
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # connect to the server
-        sock.connect(("localhost", master_port))
-
-        # send a message
-        # message = json.dumps({"hello": "world"})
-        message = json.dumps({
-            "message_type" : "register",
-            "worker_host" : "localhost",
-            "worker_port" : worker_port,
-            "worker_pid" : id
-        })
-
-        sock.sendall(message.encode('utf-8'))
-        sock.close()
-
-        if message_dict["message_type"] == "register_ack":
-
+        elif message_dict["message_type"] == "register_ack":
+            # Do something
 
 
 @click.command()
