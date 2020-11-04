ef1eef96b322b088bb8c995b61b27d416b3df30b
Thomas Smith <thsm@umich.edu>
2020-11-04 18:40:50 -0500

    Move Job class to separate file

--- mapreduce/master/__main__.py
+++ mapreduce/master/__main__.py
@@ -4,6 +4,7 @@ import json
 import socket
 import time
 import click
+from mapreduce.master.job import Job
 import mapreduce.utils
 from pathlib import Path 
 from queue import Queue
@@ -46,77 +47,6 @@ def round_robin_workers(tasks, executor):
                 if shutdown:
                     return False
         return utils.round_robin(tasks, ready_workers)
-
-
-class Job:
-    _next_id = 0
-    _current = None
-
-    def current():
-        return Job._current
-
-
-    def __init__(
-            self,
-            input_dir,
-            output_dir,
-            mapper_exec,
-            reducer_exec,
-            num_mappers,
-            num_reducers):
-        self._status = "waiting"
-        self._input_dir = input_dir
-        self._output_dir = output_dir
-        self._mapper_exec = mapper_exec
-        self._reducer_exec = reducer_exec
-        self._num_mappers = num_mappers
-        self._num_reducers = num_reducers
-
-        self._id = Job._next_job_id
-        Job._next_id += 1
-
-        logging.info(f"Master: Received job {self._id}: {input_dir} "
-                     f"{output_dir} {mapper_exec} {reducer_exec} "
-                     f"{num_mappers} {num_reducers}"
-                    )
-
-        self._folder = Path("tmp")/f"job-{job_id}"
-
-        self._folder.mkdir()
-        (self._folder/"mapper-output").mkdir()
-        (self._folder/"grouper-output").mkdir()
-        (self._folder/"reducer-output").mkdir()
-
-    def start(self):
-        logging.info(f"Master: Starting job {self._id}")
-
-        assert(Job._current is None)
-        Job._current = self
-
-        self.status = "started"
-        self.mapping()
-
-    def mapping(self):
-        # TODO: Partition input files for this stage
-        logging.info(f"Master: Starting mapping stage for job {self._id}")
-
-    def grouping(self):
-        # TODO
-        logging.info(f"Master: Starting grouping stage for job {self._id}")
-
-    def reducing(self):
-        # TODO
-        logging.info(f"Master: Starting reducing stage for job {self._id}")
-    
-    def cleanup(self):
-        # TODO
-        logging.info(f"Master: Finishing job {self._id}")
-        self.status = "finished"
-        Job._current = None
-
-    def status_update(self, worker_status):
-        # TODO
-        pass
 
 
 def start_job(msg):
