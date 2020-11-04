fe117d5cfd0a6be1ee725c0d1f6f19ecc7b21ace
Unser Hashsham <unserh@umich.edu>
2020-11-04 18:53:03 -0500

    Add initial master worker heartbeat implementation

--- mapreduce/master/__main__.py
+++ mapreduce/master/__main__.py
@@ -14,9 +14,25 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class Master:
+    # Whether or not shutdown message has been received
+    shutdown = False
+    # Dictionary of registered workers, key is pid and value is dict of worker info
+    # TODO: make this an ordered dict
+    workers = {
+        # [pid]: {
+        #   host: 
+        #   port:
+        #   status:
+        #   last_hb_received:
+        # },
+    }
+
     def __init__(self, port):
         logging.info("Starting master:%s", port)
         logging.info("Master:%s PWD %s", port, os.getcwd())
+
+        # Initialize port class variable
+        self.port = port
 
         # Create clean folder to store server results 
         p = Path('tmp')
@@ -24,58 +40,110 @@ class Master:
         for path in p.glob("job-*"):
             shutil.rmtree(path)
         
-        # listen for worker hb
+        # Listen for worker hb's
+        hb_thread = threading.Thread(target=self.heartbeat)
+        hb_thread.start()
 
         # Listen for messages on master's port
-        listen(port)
+        listen_thread = threading.Thread(target=self.listen)
+        listen_thread.start()
 
 
-def listen(port):
-    """Wait on a message from a socket OR a shutdown signal."""
+    def listen():
+        """Wait on a message from a socket OR a shutdown signal."""
 
-    # Create socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("localhost", port))
-    sock.listen()
+        # Create socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", self.port))
+        sock.listen()
 
-    # Socket accept() and recv() will block for a maximum of 1 second
-    sock.settimeout(1)
+        # Socket accept() and recv() will block for a maximum of 1 second
+        sock.settimeout(1)
 
-    # Accept connections for messages until shutdown message
-    shutdown = False 
-    while not shutdown:
-        # Listen for a connection for 1s.
-        try:
-            clientsocket, address = sock.accept()
-        except socket.timeout:
-            continue
-        print("Connection from", address[0])
-
-        # Receive data chunks
-        message_chunks = []
-        while True:
+        # Accept connections for messages until shutdown message
+        
+        while not self.shutdown:
+            # Listen for a connection for 1s.
             try:
-                data = clientsocket.recv(4096)
+                clientsocket, address = sock.accept()
             except socket.timeout:
                 continue
-            if not data:
-                break
-            message_chunks.append(data)
-        clientsocket.close()
+            print("Connection from", address[0])
 
-        # Parse message chunks into JSON data
-        message_bytes = b''.join(message_chunks)
-        message_str = message_bytes.decode("utf-8")
-        try:
-            msg = json.loads(message_str)
-        except JSONDecodeError:
-            continue
+            # Receive data chunks
+            message_chunks = []
+            while True:
+                try:
+                    data = clientsocket.recv(4096)
+                except socket.timeout:
+                    continue
+                if not data:
+                    break
+                message_chunks.append(data)
+            clientsocket.close()
 
-        # Handle message depending on type
-        if msg["message_type"] == "shutdown":
-            shutdown = True
+            # Parse message chunks into JSON data
+            message_bytes = b''.join(message_chunks)
+            message_str = message_bytes.decode("utf-8")
+            try:
+                msg = json.loads(message_str)
+            except JSONDecodeError:
+                continue
 
+            # Handle message depending on type
+            if msg["message_type"] == "shutdown":
+                self.shutdown = True
+
+
+    def heartbeat():
+        """Manage heartbeats for registered workers."""
+
+        # Create socket on master port - 1
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", self.port - 1))
+        sock.listen()
+
+        # Socket accept() and recv() will block for a maximum of 1 second
+        sock.settimeout(1)
+
+        while not self.shutdown:
+            # Check if any workers are dead
+            for pid, worker_info in workers:
+                if time.time() - worker_info["last_hb_received"] > 10:
+                    worker_info["status"] = "dead"
+
+            # Listen for a connection for 1s.
+            try:
+                clientsocket, address = sock.accept()
+            except socket.timeout:
+                continue
+            print("Connection from", address[0])
+
+            # Receive data chunks
+            message_chunks = []
+            while True:
+                try:
+                    data = clientsocket.recv(4096)
+                except socket.timeout:
+                    continue
+                if not data:
+                    break
+                message_chunks.append(data)
+            clientsocket.close()
+
+            # Parse message chunks into JSON data
+            message_bytes = b''.join(message_chunks)
+            message_str = message_bytes.decode("utf-8")
+            try:
+                msg = json.loads(message_str)
+            except JSONDecodeError:
+                continue
+
+            # Handle message depending on type
+            if msg["message_type"] == "heartbeat" and msg["worker_pid"] in workers:
+                workers[msg["worker_pid"]]["last_hb_received"] = time.time()
 
 @click.command()
 @click.argument("port", nargs=1, type=int)
--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -4,6 +4,7 @@ import json
 import time
 import click
 import mapreduce.utils
+import threading
 
 
 # Configure logging
@@ -11,76 +12,98 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class Worker:
+    # Tracks whether or not to shutdown
+    shutdown = False 
+
     def __init__(self, master_port, worker_port):
         logging.info("Starting worker:%s", worker_port)
         logging.info("Worker:%s PWD %s", worker_port, os.getcwd())
 
+        # Set port class variables
+        self.port = worker_port
+        self.master_port = master_port
+
         # Listen for messages on worker's port
-        thread = threading.Thread(target=listen, args=(worker_port,))
-        thread.start()
+        listen_thread = threading.Thread(target=self.listen)
+        listen_thread.start()
+
+        # Initiate hb before registering - storing Timer in case we need to cancel is on shutdown
+        self.heartbeat = threading.Timer(2, send_heartbeat)
 
         # Connect to the server
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("localhost", master_port))
+        sock.connect(("localhost", self.master_port))
 
         # Send register message to master
         message = json.dumps({
             "message_type" : "register",
             "worker_host" : "localhost",
-            "worker_port" : worker_port,
-            "worker_pid" : id
+            "worker_port" : self.port,
+            "worker_pid" : os.getpid(),
         })
         sock.sendall(message.encode('utf-8'))
         sock.close()
 
-def listen(port):
-    """Wait on a message from a socket OR a shutdown signal."""
 
-    # Create socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("localhost", port))
-    sock.listen()
+    def listen():
+        """Wait on a message from a socket OR a shutdown signal."""
 
-    # Socket accept() and recv() will block for a maximum of 1 second
-    sock.settimeout(1)
+        # Create socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", self.port))
+        sock.listen()
 
-    # Accept connections for messages until shutdown message
-    shutdown = False 
-    while not shutdown:
-        # Listen for a connection for 1s
-        try:
-            clientsocket, address = sock.accept()
-        except socket.timeout:
-            continue
-        print("Connection from", address[0])
+        # Socket accept() and recv() will block for a maximum of 1 second
+        sock.settimeout(1)
 
-        # Receive data chunks
-        message_chunks = []
-        while True:
+        # Accept connections for messages until shutdown message
+        while not self.shutdown:
+            # Listen for a connection for 1s
             try:
-                data = clientsocket.recv(4096)
+                clientsocket, address = sock.accept()
             except socket.timeout:
                 continue
-            if not data:
-                break
-            message_chunks.append(data)
-        clientsocket.close()
+            print("Connection from", address[0])
 
-        # Parse message chunks into JSON data
-        message_bytes = b''.join(message_chunks)
-        message_str = message_bytes.decode("utf-8")
-        try:
-            msg = json.loads(message_str)
-        except JSONDecodeError:
-            continue
+            # Receive data chunks
+            message_chunks = []
+            while True:
+                try:
+                    data = clientsocket.recv(4096)
+                except socket.timeout:
+                    continue
+                if not data:
+                    break
+                message_chunks.append(data)
+            clientsocket.close()
 
-        # Handle message depending on type
-        if msg["message_type"] == "shutdown":            
-            shutdown = True
-        elif message_dict["message_type"] == "register_ack":
-            # Do something
+            # Parse message chunks into JSON data
+            message_bytes = b''.join(message_chunks)
+            message_str = message_bytes.decode("utf-8")
+            try:
+                msg = json.loads(message_str)
+            except JSONDecodeError:
+                continue
 
+            # Handle message depending on type
+            if msg["message_type"] == "shutdown":            
+                self.shutdown = True
+
+            elif message_dict["message_type"] == "register_ack":
+                # Do something
+
+    def send_heartbeat()
+        # Open connection to master port - 1
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect(("localhost", self.master_port - 1))
+
+        message = json.dumps({
+            "message_type" : "heartbeat",
+            "worker_pid" : os.getpid(),
+        })
+        sock.sendall(message.encode('utf-8'))
+        sock.close()
 
 @click.command()
 @click.argument("master_port", nargs=1, type=int)
