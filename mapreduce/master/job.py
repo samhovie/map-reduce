6b6290d31a44caece6c1c1c4eacd460488a9445b
Unser Hashsham <unserh@umich.edu>
2020-11-11 19:53:01 -0500

    Init implementation of mapping assignment

--- mapreduce/master/job.py
+++ mapreduce/master/job.py
@@ -14,7 +14,8 @@ class Job:
             reducer_exec,
             num_mappers,
             num_reducers,
-            workers):
+            workers,
+            signals):
         self._status = "waiting"
         self._input_dir = Path(input_dir)
         self._output_dir = Path(output_dir)
@@ -23,6 +24,7 @@ class Job:
         self._num_mappers = num_mappers
         self._num_reducers = num_reducers
         self._workers = workers
+        self._signals = signals
 
         self._id = Job._next_id
         Job._next_id += 1
@@ -45,12 +47,12 @@ class Job:
 
         self._status = "started"
         self.mapping()
+        # TODO: Maybe more stuff here
 
     def mapping(self):
         logging.info(f"Master: Starting mapping stage for job {self._id}")
         partition = self.partition_input()
 
-        # ready_workers = [worker for worker in self._workers.values() if worker["status"] == "ready"]
         """
         TODO:
          - Assign as many tasks as we can to available workers
@@ -58,10 +60,39 @@ class Job:
          - If there are too many workers, don't assign all of them (we don't need them all)
         """
 
-        for task in partition:
-            # Assign task to a worker if one is available
-            for worker in self._workers.values():
-                # if worker is ready, assign task and break
+        # Each job is a tuple containing the list of input files, the PID of the worker
+        # assigned to the job, and whether the job is finished.
+        job_list = [(job, None, False) for job in partition]
+
+        while len([job for job, pid, completed in job_list if not completed]) != 0:
+            # TODO: How do we get `shutdown` from master?
+            if self._signals["shutdown"]:
+                logging.info("Shutting down in mapping stage.")
+                return False
+            for i, (job, worker_pid, completed) in enumerate(job_list):
+                if completed:
+                    continue
+                elif worker_pid is not None and self._workers[worker_pid]["status"] == "ready":
+                    # The worker has completed this job.
+                    job_list[i] = (job, self._workers[worker_pid], True)
+                elif worker_pid is None:
+                    for worker in self._workers.values():
+                        if worker["status"] == "ready":
+                            worker["status"] = "busy"
+                            mapreduce.utils.send_message({
+                                "message_type": "new_worker_job",
+                                "input_files": [str(file) for file in job],
+                                "executable": self._mapper_exec,
+                                "output_directory": str(self._output_dir),
+                                "worker_pid": worker_pid,
+                            }, worker["host"], worker["port"])
+                            break
+                elif self._workers[worker_pid]["status"] == "dead":
+                    job_list[i] = (job, None, False)
+
+            time.sleep(0.1)
+        
+        logging.info("Mapping stage complete.")
 
 
     def grouping(self):
@@ -76,10 +107,6 @@ class Job:
         # TODO
         logging.info(f"Master: Finishing job {self._id}")
         self.status = "finished"
-
-    def status_update(self, worker_status):
-        # TODO
-        pass
 
     def partition_input(self):
         assert(self._num_mappers != 0)
